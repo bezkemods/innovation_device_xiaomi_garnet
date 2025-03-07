514cf6668839175e5e59cbae516c6a87445a1a3d
basamaryan <basam.aryan@gmail.com>
2025-03-07 15:43:52 -0300

    garnet: rootdir: Remove /data/vendor/dolby on upgrade
    Based on:
    adithya2306/android_device_xiaomi_lisa@09e0c6a
    adithya2306/android_device_xiaomi_K9-sepolicy@346519f
    
    Change-Id: I2ac8f83899a0866975aa6cbce26da9670b16843d
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Kleidione Freitas <kleidione@gmail.com>

--- device.mk
+++ device.mk
@@ -205,7 +205,8 @@ PRODUCT_PACKAGES += \
     ueventd.qcom.rc
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
+    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom \
+    $(LOCAL_PATH)/rootdir/bin/init.dolby_fix.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.dolby_fix.sh
 
 # IPACM
 PRODUCT_PACKAGES += \
