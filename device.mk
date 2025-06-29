dc9f1c7585d85f456398533b580ad9b31fd86cf7
bezke <bezke1@gmail.com>
2025-06-29 10:36:16 +0200

    garnet: dalvik: Optimize heap configuration for 8-12GB RAM devices
    - Switch from 6GB to 8GB dalvik heap configuration
    - Improves memory allocation for high-RAM devices
    - Better performance for memory-intensive applications
    - Aligns heap size with target device specifications

--- device.mk
+++ device.mk
@@ -13,8 +13,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_ven
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
-# Setup dalvik vm configs
-$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+# Setup dalvik vm configs for 8-12GB RAM devices
+$(call inherit-product, frameworks/native/build/phone-xhdpi-8192-dalvik-heap.mk)
 
 # Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
