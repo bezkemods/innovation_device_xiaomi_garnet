PixelOS-Devices/android_device_xiaomi_garnet

garnet: Move miuicam includes to the end
Change-Id: I24fb5e842ea15638e116e29b16768f7cc5157a30

BoardConfig.mk
@@ -8,9 +8,6 @@ DEVICE_PATH := device/xiaomi/garnet
 
 BUILD_BROKEN_DUP_RULES := true
 
-# MiuiCamera
--include device/xiaomi/garnet-miuicamera/BoardConfig.mk
-
 # A/B
 AB_OTA_PARTITIONS := \
     boot \
@@ -241,3 +238,6 @@ WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Vendor
 include vendor/xiaomi/garnet/BoardConfigVendor.mk
+
+# Vendor MiuiCamera
+-include device/xiaomi/garnet-miuicamera/BoardConfig.mk

device.mk
@@ -16,9 +16,6 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
-# MiuiCamera
-$(call inherit-product-if-exists, device/xiaomi/garnet-miuicamera/device.mk)
-
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
@@ -463,3 +460,6 @@ PRODUCT_COPY_FILES += \
 
 # Vendor
 $(call inherit-product, vendor/xiaomi/garnet/garnet-vendor.mk)
+
+# Vendor MiuiCamera
+$(call inherit-product-if-exists, device/xiaomi/garnet-miuicamera/device.mk)

bezkemods/innovation_device_xiaomi_garnet

BoardConfig.mk
@@ -8,9 +8,6 @@ DEVICE_PATH := device/xiaomi/garnet
 
 BUILD_BROKEN_DUP_RULES := true
 
-# Inherit from proprietary files for miuicameraAdd commentMore actionsAdd commentMore actions
--include device/xiaomi/miuicamera-garnet/BoardConfig.mk
-
 # A/B
 AB_OTA_PARTITIONS := \
     boot \
@@ -247,3 +244,6 @@ WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Vendor
 include vendor/xiaomi/garnet/BoardConfigVendor.mk
+
+# Vendor MiuiCamera
+-include device/xiaomi/miuicamera-garnet/BoardConfig.mk

device.mk
@@ -4,9 +4,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Call the MiuiCamera setupAdd commentMore actionsAdd commentMore actions
-$(call inherit-product-if-exists, device/xiaomi/miuicamera-garnet/device.mk)
-
 # Enable virtual A/B OTA
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
@@ -467,3 +464,6 @@ PRODUCT_COPY_FILES += \
 
 # Vendor
 $(call inherit-product, vendor/xiaomi/garnet/garnet-vendor.mk)
+
+# Vendor MiuiCamera
+$(call inherit-product-if-exists, device/xiaomi/miuicamera-garnet/device.mk)