f0d42992460286e28d884bcdc55971bed3b3b077
bezke <bezke1@gmail.com>
2026-02-11 17:37:49 +0100

    garnet: dolby: Switch to lunaris

--- device.mk
+++ device.mk
@@ -143,11 +143,7 @@ PRODUCT_COPY_FILES += \
     hardware/qcom-caf/sm8450/display/config/snapdragon_color_libs_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/snapdragon_color_libs_config.xml
 
 # Dolby
-PRODUCT_PACKAGES += \
-    XiaomiDolby
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/dolby/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml
+$(call inherit-product, hardware/dolby/dolby.mk)
 
 # DRM
 PRODUCT_PACKAGES += \
