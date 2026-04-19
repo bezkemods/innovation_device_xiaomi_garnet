8ada8314faf7c5712738689059651ce43643ce33
bezke <bezke1@gmail.com>
2026-04-19 15:34:03 +0200

    garnet: parts: add Dirac from marble

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -32,6 +32,7 @@ import org.lineageos.settings.logcatviewer.LogcatBackgroundService;
 import org.lineageos.settings.performance.PerformanceUtils;
 import org.lineageos.settings.utils.FileUtils;
 import org.lineageos.settings.widget.XiaomiPartsWidget;
+import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.resolution.ResolutionUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
@@ -182,6 +183,13 @@ public class BootCompletedReceiver extends BroadcastReceiver {
                 Log.d(TAG, "ResolutionService started");
             } catch (Exception e) {
                 Log.e(TAG, "ResolutionService failed to start", e);
+            }
+
+            // Initialize Dirac
+            try {
+                DiracUtils.getInstance(context);
+            } catch (Exception e) {
+                Log.d(TAG, "Dirac is not present in system");
             }
         } catch (Exception e) {
             Log.e(TAG, "Failed to start services", e);
--- parts/src/org/lineageos/settings/xiaomiparts/XiaomiPartsActivity.java
+++ parts/src/org/lineageos/settings/xiaomiparts/XiaomiPartsActivity.java
@@ -28,6 +28,7 @@ import org.lineageos.settings.keyboxmanager.KeyboxManagerActivity;
 import org.lineageos.settings.ramoptimizer.RamOptimizerActivity;
 import org.lineageos.settings.refreshrate.RefreshActivity;
 import org.lineageos.settings.chargecontrol.ChargeControlActivity;
+import org.lineageos.settings.dirac.DiracActivity;
 import org.lineageos.settings.resolution.ResolutionActivity;
 
 public class XiaomiPartsActivity extends PreferenceActivity implements Preference.OnPreferenceClickListener {
@@ -51,6 +52,7 @@ public class XiaomiPartsActivity extends PreferenceActivity implements Preferenc
     private static final String KEY_THERMAL = "thermal_manager";
     private static final String KEY_CHARGE_CONTROL = "charge_control";
     private static final String KEY_RESOLUTION = "resolution_settings";
+    private static final String KEY_DIRAC = "dirac_settings";
       
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -133,6 +135,7 @@ public class XiaomiPartsActivity extends PreferenceActivity implements Preferenc
             setupPreference(KEY_THERMAL);
             setupPreference(KEY_CHARGE_CONTROL);
             setupPreference(KEY_RESOLUTION);
+            setupPreference(KEY_DIRAC);
             
             // Logcat viewer - special handling
             setupLogcatViewer();
@@ -245,6 +248,9 @@ public class XiaomiPartsActivity extends PreferenceActivity implements Preferenc
                 return true;
             } else if (KEY_RESOLUTION.equals(key)) {
                 startActivity(new Intent(this, ResolutionActivity.class));
+                return true;
+            } else if (KEY_DIRAC.equals(key)) {
+                startActivity(new Intent(this, DiracActivity.class));
                 return true;
             }
         } catch (Exception e) {
